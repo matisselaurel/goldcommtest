a25cafa1690ad0ed2d7736a7239d7c90dcdc78e5
MADDILLA <MADDILLA@Matisses-MacBook-Air.local>
2014-12-27 11:04:42 -0800

    copied twentyfourteen files into my theme

--- wp-content/themes/gctest/index.php
+++ wp-content/themes/gctest/index.php
@@ -1,53 +1,61 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>GoldenComm Developer Test Instructions</title>
-    <style type="text/css">
-        html{font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,details,figcaption,figure,footer,header,hgroup,main,nav,section,summary{display:block}audio,canvas,progress,video{display:inline-block;vertical-align:baseline}audio:not([controls]){display:none;height:0}[hidden],template{display:none}a{background:0 0}a:active,a:hover{outline:0}abbr[title]{border-bottom:1px dotted}b,strong{font-weight:700}dfn{font-style:italic}h1{font-size:2em;margin:.67em 0}mark{background:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sup{top:-.5em}sub{bottom:-.25em}img{border:0}svg:not(:root){overflow:hidden}figure{margin:1em 40px}hr{-moz-box-sizing:content-box;box-sizing:content-box;height:0}pre{overflow:auto}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em}button,input,optgroup,select,textarea{color:inherit;font:inherit;margin:0}button{overflow:visible}button,select{text-transform:none}button,html input[type=button],input[type=reset],input[type=submit]{-webkit-appearance:button;cursor:pointer}button[disabled],html input[disabled]{cursor:default}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}input{line-height:normal}input[type=checkbox],input[type=radio]{box-sizing:border-box;padding:0}input[type=number]::-webkit-inner-spin-button,input[type=number]::-webkit-outer-spin-button{height:auto}input[type=search]{-webkit-appearance:textfield;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box}input[type=search]::-webkit-search-cancel-button,input[type=search]::-webkit-search-decoration{-webkit-appearance:none}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}legend{border:0;padding:0}textarea{overflow:auto}optgroup{font-weight:700}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}.container{width:960px;margin:0 auto;padding:1.5em;}
-    </style>
-</head>
-<body>
-<div class="container">
-    <h3>Hello Matisse!</h3>
-    <p>First, notice that this file is mostly empty.</p>
-    <p><strong>Your challenge:</strong></p>
-    <ol>
-        <li>Create your own theme using the <code>gctest</code> folder under <code>/wp-content/themes/</code></li>
-        <li>Create a three page (can be more if you'd like) portfolio site using this theme<br />
-            It doesn't have to be "designey", but it should be nice and clean. Super simple is ok!
-        </li>
-        <li>All CSS has to be from scratch. No Bootstrap, Foundation, or any other CSS platforms</li>
-        <li>You can use JavaScript libraries like jQuery, but all other JavaScript has to be from scratch.</li>
-        <li>The pages to create:
-            <ul>
-                <li>Homepage
-                    <ul>
-                        <li>Must include a featured slider</li>
-                        <li>An introduction paragraph</li>
-                        <li>A featured article list</li>
-                    </ul>
-                </li>
-                <li>
-                    A portfolio showcase/gallery
-                    <ul>
-                        <li>Include 6 or so projects you've worked on in a gallery.</li>
-                        <li>Add some details for each project/item.</li>
-                    </ul>
-                </li>
-                <li>
-                    A working contact page
-                    <ul>
-                        <li>Entirely up to you how this works.</li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li><strong>There is no time limit on this test. It is up to you when it is complete. Please email <a href="mailto:aortega@goldencomm.com">Adrian</a> when it is complete.</strong></li>
-    </ol>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
 
-    <p><strong>Note:</strong> WP only requires two files for a theme: <code>index.php</code> and <code>styles.css</code>. Please expand on this, do not keep it as is.</p>
-    <h3 style="color:red">Delete this file when done!</h3>
-</div>
-</body>
-</html>
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+<?php
+	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	}
+?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+		<?php
+			if ( have_posts() ) :
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+
+				endwhile;
+				// Previous/next post navigation.
+				twentyfourteen_paging_nav();
+
+			else :
+				// If no content, include the "No posts found" template.
+				get_template_part( 'content', 'none' );
+
+			endif;
+		?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	<?php get_sidebar( 'content' ); ?>
+</div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
