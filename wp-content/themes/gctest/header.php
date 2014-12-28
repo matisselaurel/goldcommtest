665493c3f55d635154a84bf31e497a371ddf2074
MADDILLA <MADDILLA@Matisses-MacBook-Air.local>
2014-12-28 13:18:53 -0800

    Changes to index.php

--- wp-content/themes/gctest/header.php
+++ wp-content/themes/gctest/header.php
@@ -24,7 +24,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-            <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/matisse.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/matisse.js"></script>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
--- wp-content/themes/gctest/index.php
+++ wp-content/themes/gctest/index.php
@@ -24,27 +24,77 @@ get_header(); ?>
 		get_template_part( 'featured-content' );
 	}
 
-    if ( is_home() ) {
-    query_posts( "page_id=5" );
-    }
+ <?php
+if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+    // Include the featured content template.
+    get_template_part( 'featured-content' );
+}
+
+        if ( is_home() ) {
+            query_posts( "page_id=5" );
+        }
+
+$args2 = array(
+    "page_id" => 5
+);
+$wp_query2 = new WP_Query($args2);
+if ( $wp_query2->have_posts() ) :
+            // Start the Loop.
+            while ( $wp_query2->have_posts() ) : the_post();
+
+                $wp_query2->the_post();
+                //echo '<h1>'.get_the_title().'</h1>';
+                /*
+                 * Include the post format-specific template for the content. If you want to
+                 * use this in a child theme, then include a file called called content-___.php
+                 * (where ___ is the post format) and that will be used instead.
+                 */
+                 get_template_part( 'content', get_post_format() );
+
+            endwhile;
+            //wp_reset_post_data();
+            // Previous/next post navigation.
+            twentyfourteen_paging_nav();
+
+        else :
+            // If no content, include the "No posts found" template.
+            get_template_part( 'content', 'none' );
+
+        endif;
 ?>
+
+
+
+
+
+<?php //wp_reset_query(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
 		<?php
-			if ( have_posts() ) :
+		$args = array(
+		"ord" => "asc",
+		"order_by" => "title",
+		"cat" => 2,
+		"posts_per_page" => 10,
+		);
+		$wp_query = new WP_Query($args);
+			if ( $wp_query->have_posts() ) :
 				// Start the Loop.
-				while ( have_posts() ) : the_post();
+				while ( $wp_query->have_posts() ) : the_post();
 
+					$wp_query->the_post();
+					//echo '<h1>'.get_the_title().'</h1>';
 					/*
 					 * Include the post format-specific template for the content. If you want to
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					 get_template_part( 'content', get_post_format() );
 
 				endwhile;
+				//wp_reset_post_data();
 				// Previous/next post navigation.
 				twentyfourteen_paging_nav();
 
