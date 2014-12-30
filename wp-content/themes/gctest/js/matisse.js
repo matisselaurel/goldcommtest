e5f1b0b9eb73576915e076340d59d084c2af8258
MADDILLA <MADDILLA@Matisses-MacBook-Air.local>
2014-12-30 12:35:27 -0800

    Updates to frontend

--- wp-content/themes/gctest/js/matisse.js
+++ wp-content/themes/gctest/js/matisse.js
@@ -2,6 +2,7 @@
     $(document).ready(function() {
         var images = $( "#featured-images" ).html();
         $('#cbp-bislideshow').append(images);
+        $('.page-item-5').hide();
 
         $('.main-nav').on('click', function(event) {
             event.preventDefault();
